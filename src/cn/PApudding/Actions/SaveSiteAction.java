6356c323905f2a681d55bc47698dce2dc9a2645f
Pudding Add pudding <hu1song2tao3@gmail.com>
2018-12-25 15:30:57 +0800

    站点保存测试完成

---
+++ src/cn/PApudding/Actions/SaveSiteAction.java
@@ -0,0 +1,59 @@
+package cn.PApudding.Actions;
+
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import cn.PApudding.Beans.WebLink;
+import cn.PApudding.Beans.WebPage;
+import cn.PApudding.Dao.HibernateUtils;
+import cn.PApudding.SiteGenerator.SiteJspGenerator;
+
+/**
+ * 请求保存创建的站点时的Action
+ * 
+ * @author PApudding
+ *
+ */
+public class SaveSiteAction extends ActionSupport {
+	
+	@Override
+	public String execute() throws Exception {
+		//获取Http请求对象
+		HttpServletRequest request = ServletActionContext.getRequest();
+		//获取项目在服务器文件系统下的真实路径
+		String servletRealPath = ServletActionContext.getServletContext().getRealPath(".");
+		//获取全部请求的键字段
+		Enumeration<String> sitePageNames = request.getParameterNames();
+		//建立WebPage对象和WebLink对象引用
+		WebPage webPage;
+		WebLink webLink; 
+		//开始遍历以获取请求键对应的值，键值对内容格式：键：站点页面名|值：数据库中页面名
+		while (sitePageNames.hasMoreElements()) {
+			//获取当前键
+			String sitePageName = (String) sitePageNames.nextElement();
+			//获取当前值
+			String pageNameInDB = request.getParameter(sitePageName);
+			//跟据获取到的值，从数据库中取得WebPage对象
+			webPage = (WebPage) HibernateUtils.getSingleObjectBySql("from WebPage where pageName = '"+pageNameInDB+"'");
+			//生成本地的JSP文件
+			SiteJspGenerator.generateJSP(servletRealPath, sitePageName, webPage.getPageFileCode());
+			
+			//建立WebLink对象
+			webLink = new WebLink();
+			//设置该WebLink对象对应的数据源（作用域）
+			webLink.setLinkField("NavBarDefault");
+			//设置链接名
+			webLink.setLinkName(sitePageName);
+			//设置链接指向地址
+			webLink.setLinkUrl(ServletActionContext.getRequest().getContextPath()+"/enterMainSite_"+sitePageName);
+			//将该WebLink对象保存到数据库中
+			HibernateUtils.saveObject(webLink);
+		}
+		return SUCCESS;
+	}
+}
--- src/cn/PApudding/SiteGenerator/SiteJspGenerator.java
+++ src/cn/PApudding/SiteGenerator/SiteJspGenerator.java
@@ -7,25 +7,40 @@ import java.io.IOException;
 
 /**
  * 用于生成JSP页面
+ * 
  * @author PApudding
  *
  */
 public class SiteJspGenerator {
-	private static String beyongHTML = "<%@ page language=\"java\" contentType=\"text/html; charset=UTF-8\"\r\n" + 
-			"    pageEncoding=\"UTF-8\"%>\r\n" + 
-			"<!DOCTYPE html>";
-	public static void generateJSP(String ServletRealPath,String jspName,String Content) throws IOException {
-		File file = new File(ServletRealPath+"\\jsps\\MainSiteJsp\\"+jspName);
-		if(!file.exists()) {
+	private static String beyongHTML = "<%@ page language=\"java\" contentType=\"text/html; charset=UTF-8\"\r\n"
+			+ "    pageEncoding=\"UTF-8\"%>\r\n" + "<!DOCTYPE html>";
+
+	/**
+	 * 生成站点JSP文件的方法
+	 * 
+	 * @param ServletRealPath 服务器的真实路径
+	 * @param jspName
+	 * @param Content
+	 * @throws IOException
+	 */
+	public static void generateJSP(String ServletRealPath, String jspName, String Content) throws IOException {
+//		File file = new File(ServletRealPath+"\\jsps\\MainSiteJsp\\"+jspName);
+		// 使用系统无关的File.separatorChar(路径分隔符)win:\\,linux:/
+		File file = new File(ServletRealPath + File.separatorChar + "jsps" + File.separatorChar + "MainSiteJsp"
+				+ File.separatorChar + jspName + ".jsp");
+		// 判断文件是否存在，不存在则创建文件
+		if (!file.exists()) {
 			file.createNewFile();
 		}
+		// 文件字符输出流
 		FileWriter fileWriter = new FileWriter(file);
+		// 带缓冲的输出流
 		BufferedWriter bw = new BufferedWriter(fileWriter);
+		// 写入文件
 		bw.write(beyongHTML);
 		bw.newLine();
 		bw.write(Content);
 		bw.close();
 	}
-	
-	
+
 }
